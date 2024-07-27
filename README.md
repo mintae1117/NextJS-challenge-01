43d26cd3679d938071a3108a93be6482977590d1
mintaeKim <peterzzo@naver.com>
2024-07-27 20:00:28 +0900

    eslintrc.json file rules add

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-## NextJS 14 Challenge 01
+# NextJS 14 Challenge 01
+
+## eslint error => eslintrc.json file => +
+<p>"rules": {
+<p>    "react/no-unescaped-entities": 0 
+<p> },
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,9 @@
 
 export default function AboutUs() {
     return (
-      <div>
-        <h1>About us!</h1>
-        <p>Welcom to mintae's challenge project</p>
-      </div>
+        <div>
+            <h1>About us!</h1>
+            <p>Welcom to mintae's challenge project</p>
+        </div>
     );
   }
